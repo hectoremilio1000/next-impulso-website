a0d22ca2cdf32867609a2301ef4808f3b7ad3ee0
jampierzcode <jampierv127@gmail.com>
2025-02-28 14:23:15 -0500

    google tag manager configurado correctamente

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -8,8 +9,33 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Rubik:wght@900&display=swap"
           rel="stylesheet"
         />
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M676V953');
+          `,
+          }}
+        />
       </Head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M676V953"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
--- pages/layout.js
+++ pages/layout.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { GoogleTagManager } from "@next/third-parties/google";
 import { AntdRegistry } from "@ant-design/nextjs-registry"; // Ajusta el import si es necesario
+import Script from "next/script";
 
 const RootLayout = ({ children }) => (
   <html lang="en">
-    <GoogleTagManager gtmId="GTM-M676V953" />
     <body>
       <AntdRegistry>{children}</AntdRegistry>
     </body>
