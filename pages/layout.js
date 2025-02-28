a11003bebb1dc1b5e6653c90284c0c3a130c3cae
jampierzcode <jampierv127@gmail.com>
2025-02-27 22:04:59 -0500

    google tag manager agregao

--- pages/layout.js
+++ pages/layout.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { GoogleTagManager } from "@next/third-parties/google";
 import { AntdRegistry } from "@ant-design/nextjs-registry"; // Ajusta el import si es necesario
 
 const RootLayout = ({ children }) => (
   <html lang="en">
+    <GoogleTagManager gtmId="GTM-M676V953" />
     <body>
       <AntdRegistry>{children}</AntdRegistry>
     </body>
